f4033d1bb2a8a472e7188eb5697fd6f809bfe338
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-03-27 13:34:19 +0530

    Update Profile

--- public_html/Company/js/profile.js
+++ public_html/Company/js/profile.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     console.log(localStorage.getItem("companyId"));
     $.get("http://localhost:4000/company/profile?companyId=" + localStorage.getItem("companyId"), function (data) {
         if (data.isSuccess) {
+            for (key in data.keys)
             $('#name').text(data.row.name);
             $('#email').text(data.row.email);
             $('#id').text(data.row.id);
