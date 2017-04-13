9da6e270968732d8a0ffff38f0e9716d8f317b5a
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-04-13 17:23:30 +0530

    Update jobs

--- public_html/Company/js/jobs.js
+++ public_html/Company/js/jobs.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
     })
 
     $("#newJobActive").click(function () {
-        let x=$("#showWhenActiveTrue");
-        if(x.css("display")==="none"){
-            x.css("display","block");
-        }else{
-            x.css("display","none");
+        let x = $("#showWhenActiveTrue");
+        if (x.css("display") === "none") {
+            x.css("display", "block");
+        } else {
+            x.css("display", "none");
         }
     })
 
@@ -20,7 +20,19 @@ $(document).ready(function () {
     $("#addNewJobButton").click(function () {
         //TODO 1
         //Post request to add a new job
-
+        data = {
+            title: $("#newJobTitle").val(),
+            description: $("#newJobDescription").val(),
+            skills: $("#newjobskills").val(),
+            jobType: $("input[name='jobType']:checked").val(),
+            location: $("#newjoblocation").val(),
+            stipend: $("#newjobstipend").val(),
+            active: $("#newJobActive").val(),
+            startDate: $("#newjobstartdate").val(),
+            endDate: $("#newJobEndDate").val()
+        };
+        console.log(data);
+        // $.post()
 
         //TODO 2
         //Add data to html
